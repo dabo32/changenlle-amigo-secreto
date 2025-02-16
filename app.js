197fbe76fceb7e0491563e936889c444f79387f3
David Santiago <dasabo30@gmail.com>
2025-02-15 22:14:35 -0500

    Funcion Sortear Amigo

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-let amigos = [];
+const amigos = [];
+
 
 function agregarAmigo() {
     let nuevoAmigo = document.getElementById("amigo").value;
@@ -15,11 +16,24 @@ function agregarAmigo() {
 }
 
 function actualizarListaAmigos() {
-    lista = document.getElementById("listaAmigos");
+    let lista = document.getElementById("listaAmigos");
     lista.innerHTML = "";
     for (let index = 0; index < amigos.length; index++) {
         const li = document.createElement('li');
         li.innerHTML = amigos[index];
         lista.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    let inputResultado = document.getElementById("resultado");
+    //Valida 2 o más amigos para realizar el sorteo
+    if ( amigos.length <= 1){
+        alert("Lista de amigos incompleta, por favor ingrese al menos dos amigos");
+        //Agreagr al input de amigo una aleta en rojo
+    } else {
+        let numAleatorio = Math.floor(Math.random() * amigos.length);
+        let amigoAleatorio = amigos[numAleatorio];
+        inputResultado.innerHTML = amigoAleatorio;
     }
 }
